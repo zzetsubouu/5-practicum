078ff868c56ffad95e8bbdeab7156b86449d115f
zzetsubouu <154281553+zzetsubouu@users.noreply.github.com>
2024-10-23 00:21:19 +0300

    Update Program.cs

--- 5-practicum/Program.cs
+++ 5-practicum/Program.cs
@@ -15,76 +15,41 @@ namespace _5_practicum
         static void Main()
         {
             //1 задание
-            //try
-            //{
-            //    Console.Write("a=");
-            //    int a = int.Parse(Console.ReadLine());
-            //    Console.Write("b=");
-            //    int b = int.Parse(Console.ReadLine());
-            //    Console.Write("c=");
-            //    int c = int.Parse(Console.ReadLine());
-            //    double discriminant = Math.Pow(b, 2) - 4 * a * c;
-            //    if (discriminant > 0)
-            //    {
-            //        Console.WriteLine("2 корня");
-            //        double x1 = (-b + Math.Sqrt(discriminant)) / 2 * a;
-            //        double x2 = (-b - Math.Sqrt(discriminant)) / 2 * a;
-            //        Console.WriteLine($"x1={x1} \t x2={x2}");
-            //    }
-            //    else if (discriminant == 0)
-            //    {
-            //        Console.WriteLine("1 корень");
-            //        double x = -b / 2 * a;
-            //        Console.WriteLine($"x1={x}");
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("нет решений");
-            //    }
-            //    Console.ReadKey();
-            //}
-            //catch (DivideByZeroException)
-            //{
-            //    Console.WriteLine("Возникло исключение DivideByZeroException");
-            //}
-            //catch (Exception ex)
-            //{
-            //    Console.WriteLine($"Исключение: {ex.Message}");
-            //    Console.WriteLine($"Метод: {ex.TargetSite}");
-            //    Console.WriteLine($"Трассировка стека: {ex.StackTrace}");
-            //}
-
-            //2 задание
             try
             {
-                Console.Write("1 сторона=");
+                Console.Write("a=");
                 int a = int.Parse(Console.ReadLine());
-                if (a == 0)
-                {
-                    throw new ArgumentException("треугольника нет");
-                }
-                Console.Write("2 сторона=");
+                Console.Write("b=");
                 int b = int.Parse(Console.ReadLine());
-                if (b == 0)
-                {
-                    throw new ArgumentException("треугольника нет");
-                }
-                Console.Write("3 сторона=");
+                Console.Write("c=");
                 int c = int.Parse(Console.ReadLine());
-                if (c == 0)
+                double discriminant = Math.Pow(b, 2) - 4 * a * c;
+                if (discriminant > 0)
                 {
-                    throw new ArgumentException("треугольника нет");
+                    Console.WriteLine("2 корня");
+                    double x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+                    double x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+                    Console.WriteLine($"x1={x1:N2} \t x2={x2:N2}");
                 }
-                if (a<c+b&&b<a+c&&c<a+b)
+                else if (discriminant == 0)
                 {
-                    double p = (a + b + c) / 2;
-                    double s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
-                    Console.WriteLine(s);
+                    Console.WriteLine("1 корень");
+                    double x = -b / (2 * a);
+                    Console.WriteLine($"x={x:N2}");
                 }
                 else
                 {
-                    Console.WriteLine("треугольника не существует");
+                    Console.WriteLine("нет решений");
                 }
+                if (a == 0)
+                {
+                    throw new DivideByZeroException("a не может быть равно нулю");
+                }
+                Console.ReadKey();
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Возникло исключение DivideByZeroException");
             }
             catch (Exception ex)
             {
@@ -92,13 +57,42 @@ namespace _5_practicum
                 Console.WriteLine($"Метод: {ex.TargetSite}");
                 Console.WriteLine($"Трассировка стека: {ex.StackTrace}");
             }
-
-
+            
+            
+            
+            
+            
+            //2 задание
             //try
             //{
-            //    int x = 5;
-            //    int y = x / 0;
-            //    Console.WriteLine($"Результат: {y}");
+            //    Console.Write("1 сторона=");
+            //    int a = int.Parse(Console.ReadLine());
+            //    if (a == 0)
+            //    {
+            //        throw new ArgumentException("треугольника нет");
+            //    }
+            //    Console.Write("2 сторона=");
+            //    int b = int.Parse(Console.ReadLine());
+            //    if (b == 0)
+            //    {
+            //        throw new ArgumentException("треугольника нет");
+            //    }
+            //    Console.Write("3 сторона=");
+            //    int c = int.Parse(Console.ReadLine());
+            //    if (c == 0)
+            //    {
+            //        throw new ArgumentException("треугольника нет");
+            //    }
+            //    if (a < c + b && b < a + c && c < a + b)
+            //    {
+            //        double p = (a + b + c) / 2.0;
+            //        double s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            //        Console.WriteLine($"Площадь треугольника: {s:N2}");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("треугольника не существует");
+            //    }
             //}
             //catch (Exception ex)
             //{
